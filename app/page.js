2ad9ec42527572eda4856535bacbb0e25a8a9bbc
Ankit Kaushal <ankitkaushal288@gmail.com>
2025-05-02 01:40:26 +0530

    build fix

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/context/AuthContext';
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Home() {
   const router = useRouter();
@@ -9,9 +9,9 @@ export default function Home() {
 
   useEffect(() => {
     if (user) {
-      router.push('/game');
+      router.push("/game");
     } else {
-      router.push('/login');
+      router.push("/login");
     }
   }, [user, router]);
 
